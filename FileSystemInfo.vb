ed665b00dae8202d8d82d11129b8e62a498b5b54
Tim Rossiter <rosstimo@isu.edu>
2020-10-06 06:53:42 -0600

    Add project files.

---
+++ FileSystemInfo.vb
@@ -0,0 +1,28 @@
+﻿Module FileSystemInfo
+
+    Sub main()
+        'CurrentDir()
+        ChangeDir()
+        Console.Read()
+    End Sub
+
+    Sub CurrentDir()
+        Dim path As String
+        path = CurDir()
+        Console.WriteLine(path)
+        Try
+            Console.WriteLine(CurDir("C"c))
+            Console.WriteLine(CurDir("D"c))
+        Catch badDir As System.IO.IOException
+
+        End Try
+    End Sub
+
+    Sub ChangeDir()
+        Console.WriteLine(CurDir())
+        ChDir("..") 'up one level
+        Console.WriteLine(CurDir())
+    End Sub
+
+
+End Module
---
+++ ReadWriteAppend.vb
@@ -0,0 +1,69 @@
+Imports System
+
+Module ReadWriteAppend
+    Sub Main(args As String())
+        Console.WriteLine("Hello World!")
+    End Sub
+
+    '<Remarks>
+    'ReadFile() allows user to choose text file then reads file into TempArray
+    '</Remarks>
+    Private Sub ReadFile(ByVal fileName As String, ByRef recordData() As String)
+        Dim currentRecord As String
+        Dim fileData As String
+        Dim fileNumber As Integer = FreeFile()
+
+        Try
+            FileOpen(fileNumber, fileName, OpenMode.Input)
+            Do While Not EOF(fileNumber)
+                Input(fileNumber, currentRecord)
+                fileData &= currentRecord
+            Loop
+        Catch ex As Exception
+            'TODO: user select file if it doesn't exist
+            Console.WriteLine(ex.Message)
+        Finally
+            FileClose(fileNumber)
+        End Try
+
+        recordData = Split(fileData, "$$")
+
+    End Sub
+
+
+    Private Sub AppendFile(ByVal filename As String, newRecord As String)
+        Dim fileNumber As Integer = FreeFile()
+
+        Try
+            FileOpen(fileNumber, filename, OpenMode.Append)
+            Write(fileNumber, newRecord)
+            WriteLine(fileNumber)
+
+        Catch ex As Exception
+            'TODO: user select file if it doesn't exist
+            'handle file in use exception
+            'verify other possible exceptions           
+        Finally
+            FileClose(fileNumber)
+        End Try
+
+    End Sub
+
+    Private Sub WriteFile(ByVal filename As String, newRecord As String)
+        Dim fileNumber As Integer = FreeFile()
+
+        Try
+            FileOpen(fileNumber, filename, OpenMode.Output)
+            Write(fileNumber, newRecord)
+
+        Catch ex As Exception
+            'TODO: user select file if it doesn't exist
+            'handle file in use exception
+            'verify other possible exceptions           
+        Finally
+            FileClose(fileNumber)
+        End Try
+
+    End Sub
+
+End Module
